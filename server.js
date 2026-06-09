27b9cf0aab203cdc437c08ab64b97697c1a1df8c
ErShubham4u <connect2ershubham@gmail.com>
2026-06-09 22:18:02 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/menu/", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5288974&lng=73.8665321&restaurantId=",
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/137.0.0.0 Safari/537.36",
+          Accept: "application/json",
+        },
+      }
+    );
+
+    console.log("Status:", response.status);
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
